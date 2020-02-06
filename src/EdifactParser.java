640fbeeedccf843347d8f850ec890d27f4621cda
Colin Fleck <fleckc3@mail.dcu.ie>
2020-02-06 14:44:37 +0000

    added edifact text to be manipulated

--- src/EdifactParser.java
+++ src/EdifactParser.java
@@ -1,4 +1,6 @@
-
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * Edifact Parser class takes an Edifact message text and parses out the LOC
@@ -9,6 +11,30 @@
 public class EdifactParser {
 
 	public static void main(String[] args) {
+		
+		//creates file with edifact text
+				try {
+					FileWriter edifactText = new FileWriter("edifact.txt");
+					edifactText.write("UNA:+.? '\r\n" + 
+							"UNB+UNOC:3+2021000969+4441963198+180525:1225+3VAL2MJV6EH9IX+KMSV7HMD+CU\r\n" + 
+							"SDECU-IE++1++1'\r\n" + 
+							"UNH+EDIFACT+CUSDEC:D:96B:UN:145050'\r\n" + 
+							"BGM+ZEM:::EX+09SEE7JPUV5HC06IC6+Z'\r\n" + 
+							"LOC+17+IT044100'\r\n" + 
+							"LOC+18+SOL'\r\n" + 
+							"LOC+35+SE'\r\n" + 
+							"LOC+36+TZ'\r\n" + 
+							"LOC+116+SE003033'\r\n" + 
+							"DTM+9:20090527:102'\r\n" + 
+							"DTM+268:20090626:102'\r\n" + 
+							"DTM+182:20090527:102'");
+					edifactText.close();
+					
+				} catch (IOException e) {
+					//gives error if one occurs during the file writing
+					System.out.println("Error occurred while writing file.");
+					e.printStackTrace();
+				}
 		
 
 	}
