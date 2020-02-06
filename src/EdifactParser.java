8c3e137eeeafc28e01cddea628551170a870c0d8
Colin Fleck <fleckc3@mail.dcu.ie>
2020-02-06 14:16:29 +0000

    initial code challange git setup

---
+++ src/EdifactParser.java
@@ -0,0 +1,16 @@
+
+
+/**
+ * Edifact Parser class takes an Edifact message text and parses out the LOC
+ * segments and populates an array with the 2nd and 3rd element of each segment
+ * @author Colin
+ *
+ */
+public class EdifactParser {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
